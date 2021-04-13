49d9dbc8ca1c7b5598ad16440bedf8e9104da027
anavsingh99 <as9dw@virginia.edu>
2021-04-13 16:29:41 -0400

    Anav4

--- practice/fastapi_anav/README_anav.md
+++ practice/fastapi_anav/README_anav.md
@@ -2,9 +2,16 @@
 
 ## About
 
-The API has various geography and location-related endpoints to GET data such as:
-1) Location weather and temperature
-2) US State capitals
-3) ISO3166 code for a given country
-4) Country for a given ISO3166 code
+Anav Singh's DS3002 Project: API-based Chatbot
 
+The API has various geography and location-related endpoints to GET data.
+
+OpenWeatherMap's API has been used to gather data for temperature and forecasts. Check it out at: https://openweathermap.org/api
+
+## Endpoints and Output
+
+/weather/location - location's current weather and temperature
+/forecast/location/hour - location forecast for tomorrow at a given hour (input in 24 hour format)
+/statecap/state - capital of the US state
+/iso/country - country's ISO3166 code
+/country/iso - ISO code's associated country
--- practice/fastapi_anav/prac.py
+++ practice/fastapi_anav/prac.py
@@ -1,35 +1,35 @@
-import pyowm
+from pyowm.owm import OWM
 
-#APIKEY= '03f6f7e2a6de6ff65f3a24e46cd8930c'    #your API Key here as string
-#OpenWMap=pyowm.OWM(APIKEY)                   # Use API key to get data
-#Weather=OpenWMap.weather_at_place('London')  # give where you need to see the weather
-#Data=Weather.get_weather()                   # get out data in the mentioned location
+owm = OWM('03f6f7e2a6de6ff65f3a24e46cd8930c')
+mgr = owm.weather_manager()
+three_h_forecast = mgr.forecast_at_place('New York', '3h')
+print(three_h_forecast.weather)
 
-#temp = Data.get_temperature(unit='celsius')      # get current temparature in celsius 
-#print ("Average Temp. Currently ", temp['temp']) # get avg. tmp
-#print ("Max Temp. Currently ", temp['temp_max']) # get max tmp
-#print ("Min Temp. Currently ", temp['temp_min']) # get min tmp>>
+from pyowm.utils import timestamps
+from pyowm.owm import OWM
+owm = OWM('03f6f7e2a6de6ff65f3a24e46cd8930c')
+mgr = owm.weather_manager()
+three_h_forecaster = mgr.forecast_at_place('Charlottesville', '3h')
+tomorrow_at_five = timestamps.tomorrow(17, 0)                      # datetime object for tomorrow at 5 PM
+weather = three_h_forecaster.get_weather_at(tomorrow_at_five) 
+print(weather.status)
 
-#owm = pyowm.OWM('03f6f7e2a6de6ff65f3a24e46cd8930c')
-#mgr = owm.weather_manager()
-#obs = mgr.weather_at_place('Delhi')
-#w = obs.weather
-#print(w.status)
+from pyowm.utils import timestamps
+from pyowm.owm import OWM
+owm = OWM('03f6f7e2a6de6ff65f3a24e46cd8930c')
+mgr = owm.weather_manager()
+three_h_forecaster = mgr.forecast_at_place('Charlottesville', '3h')
+tomorrow_at_five = timestamps.tomorrow(17, 0)                      # datetime object for tomorrow at 5 PM
+weather = three_h_forecaster.weather()
+print(weather.status)
 
-def get_temp(loc: str):
-    owm = pyowm.OWM('03f6f7e2a6de6ff65f3a24e46cd8930c')
+def forecast(loc: str, hr: int):
+    owm = OWM('03f6f7e2a6de6ff65f3a24e46cd8930c')
     mgr = owm.weather_manager()
     loc2 = loc.title()
-    obs = mgr.weather_at_place(loc2)
-    w = obs.weather
-    t = w.temperature(unit='fahrenheit')
-    print(t['temp'])
+    three_h_forecaster = mgr.forecast_at_place(loc2, '3h')
+    tomorrow_at_five = timestamps.tomorrow(hr, 0)
+    weather = three_h_forecaster.get_weather_at(tomorrow_at_five) 
+    return {"Location": loc2, "Forecast For": 'Tomorrow', "24 Hour": hr , "Status": weather.status}
 
-#get_temp('new york')
-
-owm = pyowm.OWM('03f6f7e2a6de6ff65f3a24e46cd8930c')
-mgr = owm.weather_manager()
-obs = mgr.weather_at_place('London')
-w = obs.weather
-t = w.temperature(unit='fahrenheit')
-print(w)
+forecast('singapore', 8)
